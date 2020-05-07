a9d131e742724199ff208d6d56aaeb0abb2948a2
Jan Felix Langenbach <o.hase3@gmail.com>
2020-05-07 12:32:18 +0200

    Remove fmt--normalize.

--- autoload/fmt.el
+++ autoload/fmt.el
@@ -2,29 +2,6 @@
 
 (defvar fmt/formatter)
 
-(defun fmt--normalize (&optional fmt)
-  "Return FMT or `fmt/formatter' as a cons (BUF-FN . REG-FN)."
-  (unless fmt (setq fmt fmt/formatter))
-  (cond
-   ((not fmt) (error "No formatter"))
-
-   ((functionp fmt)
-    (cl-destructuring-bind (min . max) (func-arity fmt)
-      (let ((fmt-cons (cons nil nil)))
-        ;; Can `fmt' be used as buffer formatter?
-        (when (zerop min) (setf (car fmt-cons) fmt))
-        ;; Can `fmt' be used as region formatter?
-        (when (and (<= min 2) (or (symbolp max) (>= max 2)))
-          (setf (cdr fmt-cons) fmt))
-        (if (or (car fmt-cons) (cdr fmt-cons)) fmt-cons
-          (error "Wrong formatter arity: %s, %s, %s" fmt min max)))))
-
-   ((consp fmt)
-    (if (or (car fmt) (cdr fmt)) fmt
-      (error "Invalid formatter: (nil . nil)")))
-
-   (t (error "Invalid formatter: %s" fmt))))
-
 (defun fmt--classify (fmt)
   (unless fmt (error "No formatter specified"))
   (let* ((arity (func-arity fmt))
