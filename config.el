daa3fda1306c26eb1849ae046817a835d03f91b6
Jan Felix Langenbach <o.hase3@gmail.com>
2020-05-07 06:52:04 +0200

    Move lsp-format hook application to fmt/config.el.

--- config.el
+++ config.el
@@ -16,9 +16,11 @@ FN is a function that takes zero or two arguments.")
 
 (when (featurep! :tools lsp)
   (defun +fmt-lsp-mode-maybe-set-formatter-h ()
+    "Change formatter to `lsp-format' if the language server supports formatting."
     (let ((buffer (lsp-feature? "textDocument/formatting"))
           (region (lsp-feature? "textDocument/rangeFormatting")))
       (if (and buffer region)
           (setq fmt/formatter (cons #'lsp-format-buffer #'lsp-format-region))
         (when buffer (setq fmt/formatter #'lsp-format-buffer))
-        (when region (setq fmt/formatter #'lsp-format-region))))))
+        (when region (setq fmt/formatter #'lsp-format-region)))))
+  (add-hook 'lsp-mode-hook #'+fmt-lsp-mode-maybe-set-formatter-h))
