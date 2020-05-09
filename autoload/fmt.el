e42b9f0e373687fabe71873bb278063ff441d714
Jan Felix Langenbach <o.hase3@gmail.com>
2020-05-09 23:03:22 +0200

    Remove +fmt--normalize.

--- autoload/fmt.el
+++ autoload/fmt.el
@@ -14,20 +14,6 @@ If R is non-nil, FMT can be used with two arguments."
       (error "Wrong formatter arity: %s, %s" fmt arity))
     (cons buffer region)))
 
-(defun +fmt--normalize (&optional fmt)
-  "Normalize FMT to a cons (BUF-FN . REG-FN).
-BUF-FN is a function that takes no arguments or nil.
-REG-FN is a function that takes two arguments or nil.
-The expression (or BUF-FN REG-FN) always evaluates to t."
-  (unless fmt (setq fmt +fmt/formatter))
-  (cond ((null fmt) (error "No formatter specified"))
-        ((consp fmt) (if (or (car fmt) (cdr fmt)) fmt
-                       (error "Invalid formatter: (nil . nil)")))
-        ((functionp fmt) (let ((arity (+fmt--classify fmt)))
-                           (cons (and (car arity) fmt)
-                                 (and (cdr arity) fmt))))
-        (t (error "Invalid formatter: %s" fmt))))
-
 (defun +fmt--current-indentation ()
   "Return the current general indentation."
   (save-excursion
@@ -67,19 +53,18 @@ The expression (or BUF-FN REG-FN) always evaluates to t."
 (defun +fmt/buffer (&optional fmt)
   "Format the current buffer with FMT or `+fmt/formatter'."
   (interactive)
-  (cl-destructuring-bind (buf-fn . reg-fn) (+fmt--normalize fmt)
-    (if buf-fn (funcall buf-fn)
-      (funcall reg-fn (point-min) (point-max)))))
+  (if (car (+fmt--classify fmt)) (funcall fmt)
+    (funcall fmt (point-min) (point-max))))
 
 ;;;###autoload
 (defun +fmt/region (beg end &optional fmt)
   "Format the current region with FMT or `+fmt/formatter'."
   (interactive "r")
-  (cl-destructuring-bind (buf-fn . reg-fn) (+fmt--normalize fmt)
-    (if reg-fn (funcall reg-fn beg end)
-      (if (and (eq beg (point-min)) (eq end (point-max)))
-          (funcall buf-fn)
-        (+fmt--narrowed-to-region beg end (funcall buf-fn))))))
+  (if (cdr (+fmt--classify fmt)) (funcall fmt beg end)
+    (if (and (eq beg (point-min))
+             (eq end (point-max)))
+        (funcall fmt)
+      (+fmt--narrowed-to-region beg end (funcall buf-fn)))))
 
 ;;;###autoload
 (defun +fmt/dwim ()
