7761c0e8824568f8fce4084d696ca1e82f7bfdb1
Jan Felix Langenbach <o.hase3@gmail.com>
2021-02-11 18:54:19 +0100

    Update +fmt-htmltidy.

--- autoload/fmt-htmltidy.el
+++ autoload/fmt-htmltidy.el
@@ -1,19 +1,26 @@
-;;; autoload/fmt-tidy.el -*- lexical-binding: t; -*-
-;;;###if (featurep! :editor fmt)
+;;; autoload/fmt-htmltidy.el -*- lexical-binding: t; -*-
 
-(defvar tidy-config-file nil
-  "The configuration file for `tidy-format-buffer'.")
+;;; HTML Tidy - A tool to tidy down your HTML code to a clean style
+;;; https://www.html-tidy.org/
 
-(defvar tidy-xml-mode-list '(nxml-mode)
-  "A list of modes where `tidy-format-buffer' uses the “-xml” flag.")
+(defvar +fmt-htmltidy-program "tidy"
+  "The executable for `+fmt-htmltidy-format-region'.")
+
+(defvar +fmt-htmltidy-args nil
+  "List of additional arguments passed to `+fmt-htmltidy-program'.")
+
+(defvar +fmt-htmltidy-config-file nil
+  "The configuration file for `+fmt-htmltidy-program'.")
+
+(defvar +fmt-htmltidy-xml-mode-list '(nxml-mode)
+  "List of modes where `+fmt-htmltidy-format-buffer' uses the “-xml” flag.")
 
 (defun tidy-compute-args ()
-  "Compute arguments for `tidy-format-buffer'."
-  (when indent-tabs-mode (warn "HTML Tidy doesn’t support indent-tabs-mode"))
+  "Compute arguments passed to `+fmt-htmltidy-program'."
   (nconc
-   (when (memq major-mode tidy-xml-mode-list) '("-xml"))
-   (when (and tidy-config-file (file-readable-p tidy-config-file))
-     (list "-config" tidy-config-file))
+   (when (memq major-mode +fmt-htmltidy-xml-mode-list) '("-xml"))
+   (when (and +fmt-htmltidy-config-file (file-readable-p +fmt-htmltidy-config-file))
+     (list "-config" +fmt-htmltidy-config-file))
    (list
     "--quiet"            "yes"
     "--tidy-mark"        "no"
@@ -21,11 +28,12 @@
     "--indent-with-tabs" (if indent-tabs-mode "yes" "no")
     "--indent-spaces"    (if indent-tabs-mode "1" (number-to-string standard-indent))
     "--wrap"             (number-to-string fill-column)
-    "--tab-size"         (number-to-string tab-width))))
+    "--tab-size"         (number-to-string tab-width))
+   +fmt-htmltidy-args))
 
-;;;###autoload (autoload 'tidy-format-buffer "autoload/fmt-tidy" nil t)
-;;;###autoload (autoload 'tidy-format-region "autoload/fmt-tidy" nil t)
-(+fmt-define tidy
-  :program "tidy"
-  :args (tidy-compute-args)
+;;;###autoload (autoload '+fmt-htmltidy-format-buffer "autoload/fmt-htmltidy" nil t)
+;;;###autoload (autoload '+fmt-htmltidy-format-region "autoload/fmt-htmltidy" nil t)
+(+fmt-define +fmt-htmltidy
+  :program +fmt-htmltidy-program
+  :args (+fmt-htmltidy-compute-args)
   :exit-code-success-p (lambda (x) (memq x '(0 1))))
