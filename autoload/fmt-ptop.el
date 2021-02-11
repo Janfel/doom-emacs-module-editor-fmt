8f56736e654d110a865d4fb7aa4e13c3edf26a2f
Jan Felix Langenbach <o.hase3@gmail.com>
2021-02-11 21:27:41 +0100

    Update +fmt-ptop.

--- autoload/fmt-ptop.el
+++ autoload/fmt-ptop.el
@@ -1,23 +1,32 @@
 ;;; autoload/fmt-ptop.el -*- lexical-binding: t; -*-
 
-(defvar ptop-config-file
-  (expand-file-name "pascal/ptop.cfg" XDG-CONFIG-HOME)
-  "The configuration file for `ptop-format-buffer'.")
+;;; PTOP - Free Pascal Source Formatter
+;;; https://freepascal.org/tools/ptop.html
+
+(defvar +fmt-ptop-program "ptop"
+  "The executable for `+fmt-ptop-format-region'.")
+
+(defvar +fmt-ptop-args nil
+  "List of additional arguments passed to `+fmt-ptop-program'.")
+
+(defvar +fmt-ptop-config-file nil
+  "The configuration file for `+fmt-ptop-program'.")
 
 ;; TODO: Use :stdin/:stdout instead of /dev/ on Windows.
-(defun ptop-compute-args ()
-  "Compute arguments for `ptop-format-region'."
+(defun +fmt-ptop-compute-args ()
+  "Compute arguments passed to `+fmt-ptop-program'."
   (when IS-WINDOWS (error "PTOP can't be used on Windows"))
   (nconc
-   (when (and ptop-config-file (file-readable-p ptop-config-file))
-     (list "-c" ptop-config-file))
+   (when (and +fmt-ptop-config-file (file-readable-p +fmt-ptop-config-file))
+     (list "-c" +fmt-ptop-config-file))
    (list
     "-i" (number-to-string standard-indent)
     "-l" (number-to-string fill-column)
-    "/dev/stdin" "/dev/stdout")))
+    "/dev/stdin" "/dev/stdout")
+   +fmt-ptop-args))
 
-;;;###autoload (autoload 'ptop-format-buffer "autoload/fmt-ptop" nil t)
-;;;###autoload (autoload 'ptop-format-region "autoload/fmt-ptop" nil t)
-(+fmt-define ptop
-  :program "ptop"
-  :args (ptop-compute-args))
+;;;###autoload (autoload '+fmt-ptop-format-buffer "autoload/fmt-ptop" nil t)
+;;;###autoload (autoload '+fmt-ptop-format-region "autoload/fmt-ptop" nil t)
+(+fmt-define +fmt-ptop
+  :program +fmt-ptop-program
+  :args (+fmt-ptop-compute-args))
