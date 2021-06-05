30ebf4d196524089a950667b020253a4724f6f37
Jan Felix Langenbach <o.hase3@gmail.com>
2021-06-06 01:00:08 +0200

    Improve fmt-shfmt shell compatibility.

--- autoload/fmt-shfmt.el
+++ autoload/fmt-shfmt.el
@@ -9,12 +9,20 @@
 (defvar +fmt-shfmt-args '("-sr")
   "List of additional arguments passed to `+fmt-shfmt-program'.")
 
+(defvar +fmt-shfmt-shell-alist
+  '((bats  bats)
+    (mksh  pdksh mksh)
+    (posix ash dash es jsh oash posix rpm sh wsh))
+  "Alist mapping shfmt shells to lists of `sh-shell' shells.")
+
 (defun +fmt-shfmt-compute-args ()
   "Compute arguments passed to `+fmt-shfmt-program'."
   (nconc
    (and buffer-file-name (list "-filename" buffer-file-name))
-   (when (and (derived-mode-p 'sh-mode) (bound-and-true-p sh-shell))
-     (list "-ln" (symbol-name sh-shell)))
+   (when (bound-and-true-p sh-shell)
+     (when-let (shell (seq-some (lambda (c) (if (memq sh-shell (cdr c)) (car c)))
+                                +fmt-shfmt-shell-alist))
+       (list "-ln" (symbol-name shell))))
    (list "-i" (number-to-string (if indent-tabs-mode 0 standard-indent)))
    +fmt-shfmt-args))
 
